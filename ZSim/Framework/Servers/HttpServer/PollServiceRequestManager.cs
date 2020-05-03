ec8c898afdee562127686cbee2bb77b79328eac7
Zontreck <tara@zontreck.dev>
2020-05-03 14:28:11 -0700

    Update more branding to ZSim

---
+++ ZSim/Framework/Servers/HttpServer/PollServiceRequestManager.cs
@@ -0,0 +1,257 @@
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections;
+using System.Threading;
+using System.Reflection;
+using log4net;
+using ZSim.Framework.Monitoring;
+using Amib.Threading;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace ZSim.Framework.Servers.HttpServer
+{
+    public class PollServiceRequestManager
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private BlockingCollection<PollServiceHttpRequest> m_requests = new BlockingCollection<PollServiceHttpRequest>();
+        private ConcurrentQueue<PollServiceHttpRequest> m_retryRequests = new ConcurrentQueue<PollServiceHttpRequest>();
+
+        private uint m_WorkerThreadCount = 0;
+        private Thread[] m_workerThreads;
+        private Thread m_retrysThread;
+
+        private bool m_running = false;
+
+        //private SmartThreadPool m_threadPool;
+
+        public PollServiceRequestManager(
+            bool performResponsesAsync, uint pWorkerThreadCount, int pTimeout)
+        {
+            m_WorkerThreadCount = pWorkerThreadCount;
+            m_workerThreads = new Thread[m_WorkerThreadCount];
+
+            /*
+            STPStartInfo startInfo = new STPStartInfo();
+            startInfo.IdleTimeout = 30000;
+            startInfo.MaxWorkerThreads = 20;
+            startInfo.MinWorkerThreads = 1;
+            startInfo.ThreadPriority = ThreadPriority.Normal;
+            startInfo.StartSuspended = true;
+            startInfo.ThreadPoolName = "PoolService";
+
+            m_threadPool = new SmartThreadPool(startInfo);
+            */
+        }
+
+        public void Start()
+        {
+            if(m_running)
+                return;
+            m_running = true;
+            //m_threadPool.Start();
+            //startup worker threads
+            for (uint i = 0; i < m_WorkerThreadCount; i++)
+            {
+                m_workerThreads[i]
+                    = WorkManager.StartThread(
+                        PoolWorkerJob,
+                        string.Format("PollServiceWorkerThread {0}", i),
+                        ThreadPriority.Normal,
+                        true,
+                        false,
+                        null,
+                        int.MaxValue);
+            }
+
+            m_retrysThread = WorkManager.StartThread(
+                this.CheckRetries,
+                string.Format("PollServiceWatcherThread"),
+                ThreadPriority.Normal,
+                true,
+                true,
+                null,
+                1000 * 60 * 10);
+        }
+
+        private void ReQueueEvent(PollServiceHttpRequest req)
+        {
+            if (m_running)
+                m_retryRequests.Enqueue(req);
+        }
+
+        public void Enqueue(PollServiceHttpRequest req)
+        {
+            if(m_running)
+                m_requests.Add(req);
+        }
+
+        private void CheckRetries()
+        {
+            PollServiceHttpRequest preq;
+            while (m_running)
+            {
+                Thread.Sleep(100);
+                Watchdog.UpdateThread();
+                while (m_running && m_retryRequests.TryDequeue(out preq))
+                    m_requests.Add(preq);
+            }
+        }
+
+        public void Stop()
+        {
+            if(!m_running)
+                return;
+
+            m_running = false;
+
+            Thread.Sleep(100); // let the world move
+
+            foreach (Thread t in m_workerThreads)
+                Watchdog.AbortThread(t.ManagedThreadId);
+
+            //m_threadPool.Shutdown();
+
+            // any entry in m_bycontext should have a active request on the other queues
+            // so just delete contents to easy GC
+            PollServiceHttpRequest req;
+            try
+            {
+                while (m_retryRequests.TryDequeue(out req))
+                    req.DoHTTPstop();
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                while(m_requests.TryTake(out req, 0))
+                    req.DoHTTPstop();
+            }
+            catch
+            {
+            }
+            m_requests.Dispose();
+        }
+
+        // work threads
+
+        private void PoolWorkerJob()
+        {
+            PollServiceHttpRequest req;
+            while (m_running)
+            {
+                try
+                {
+                    req = null;
+                    if (!m_requests.TryTake(out req, 4500) || req == null)
+                    {
+                        Watchdog.UpdateThread();
+                        continue;
+                    }
+
+                    Watchdog.UpdateThread();
+
+                    if (!req.HttpContext.CanSend())
+                    {
+                        req.PollServiceArgs.Drop(req.RequestID, req.PollServiceArgs.Id);
+                        continue;
+                    }
+
+                    if (req.HttpContext.IsSending())
+                    {
+                        if ((Environment.TickCount - req.RequestTime) > req.PollServiceArgs.TimeOutms)
+                        {
+                            req.PollServiceArgs.Drop(req.RequestID, req.PollServiceArgs.Id);
+                        }
+                        else
+                            ReQueueEvent(req);
+                        continue;
+                    }
+
+                    if (req.PollServiceArgs.HasEvents(req.RequestID, req.PollServiceArgs.Id))
+                    {
+                        PollServiceHttpRequest nreq = req;
+                        //m_threadPool.QueueWorkItem(x =>
+                        //{
+                            try
+                            {
+                                Hashtable responsedata = nreq.PollServiceArgs.GetEvents(nreq.RequestID, nreq.PollServiceArgs.Id);
+                                nreq.DoHTTPGruntWork(responsedata);
+                            }
+                            catch (ObjectDisposedException) { }
+                            finally
+                            {
+                                nreq = null;
+                            }
+                            //return null;
+                        //}, null);
+                    }
+                    else
+                    {
+                        if ((Environment.TickCount - req.RequestTime) > req.PollServiceArgs.TimeOutms)
+                        {
+                            PollServiceHttpRequest nreq = req;
+                            //m_threadPool.QueueWorkItem(x =>
+                            //{
+                                try
+                                {
+                                    nreq.DoHTTPGruntWork(nreq.PollServiceArgs.NoEvents(nreq.RequestID, nreq.PollServiceArgs.Id));
+                                }
+                                catch (ObjectDisposedException) { }
+                                finally
+                                {
+                                    nreq = null;
+                                }
+                                //return null;
+                            //}, null);
+                        }
+                        else
+                        {
+                            ReQueueEvent(req);
+                        }
+                    }
+                }
+                catch (ThreadAbortException)
+                {
+                    Thread.ResetAbort();
+                    // Shouldn't set this to 'false', the normal shutdown should cause things to exit
+                    // but robust is still not normal neither is mono
+                    m_running = false;
+                }
+                catch (Exception e)
+                {
+                    m_log.ErrorFormat("Exception in poll service thread: " + e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ ZSim/Server/Handlers/Neighbour/NeighbourHandlers.cs
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Net;
+using System.Text;
+
+using ZSim.Server.Base;
+using ZSim.Server.Handlers.Base;
+using ZSim.Services.Interfaces;
+using ZSim.Framework;
+using ZSim.Framework.Servers.HttpServer;
+using GridRegion = OpenSim.Services.Interfaces.GridRegion;
+
+using OpenMetaverse;
+using OpenMetaverse.StructuredData;
+using log4net;
+
+
+namespace ZSim.Server.Handlers.Neighbour
+{
+    public class NeighbourSimpleHandler : SimpleStreamHandler
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private INeighbourService m_NeighbourService;
+        private IAuthenticationService m_AuthenticationService;
+
+        public NeighbourSimpleHandler(INeighbourService service, IAuthenticationService authentication) :
+                base("/region")
+        {
+            m_NeighbourService = service;
+            m_AuthenticationService = authentication;
+        }
+
+        protected override void ProcessRequest(IOSHttpRequest httpRequest, IOSHttpResponse httpResponse)
+        {
+            httpResponse.KeepAlive = false;
+
+            if (m_NeighbourService == null)
+            {
+                httpResponse.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return;
+            }
+
+            switch (httpRequest.HttpMethod)
+            {
+                case "POST":
+                {
+                    OSDMap args = RestHandlerUtils.DeserializeOSMap(httpRequest);
+                    if (args == null)
+                    {
+                        httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+                        httpResponse.RawBuffer = Util.UTF8.GetBytes("false");
+                        return;
+                    }
+
+                    if (RestHandlerUtils.GetParams(httpRequest.UriPath, out UUID regionID, out ulong regionHandle, out string action)
+                        || regionID == UUID.Zero)
+                    {
+                        m_log.InfoFormat("[RegionPostHandler]: Invalid parameters for neighbour message {0}", httpRequest.UriPath);
+                        httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+                        return;
+                    }
+                    ProcessPostRequest(args, httpRequest, httpResponse, regionID);
+                    break;
+                }
+                case "GET":
+                case "PUT":
+                case "DELETE":
+                    httpResponse.StatusCode = (int)HttpStatusCode.NotImplemented;
+                    return;
+                default:
+                {
+                    httpResponse.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                    return;
+                }
+            }
+        }
+
+        // TODO: unused: private bool m_AllowForeignGuests;
+        protected void ProcessPostRequest(OSDMap args, IOSHttpRequest httpRequest, IOSHttpResponse httpResponse, UUID regionID)
+        {
+            byte[] result = new byte[0];
+
+            if (m_AuthenticationService != null)
+            {
+                // Authentication
+                string authority = string.Empty;
+                string authToken = string.Empty;
+                if (!RestHandlerUtils.GetAuthentication(httpRequest, out authority, out authToken))
+                {
+                    m_log.InfoFormat("[RegionPostHandler]: Authentication failed for neighbour message");
+                    httpResponse.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    return;
+                }
+                // TODO: Rethink this
+                //if (!m_AuthenticationService.VerifyKey(regionID, authToken))
+                //{
+                //    m_log.InfoFormat("[RegionPostHandler]: Authentication failed for neighbour message {0}", path);
+                //    httpResponse.StatusCode = (int)HttpStatusCode.Forbidden;
+                //    return result;
+                //}
+                m_log.DebugFormat("[RegionPostHandler]: Authentication succeeded for {0}", regionID);
+            }
+
+            // retrieve the regionhandle
+            ulong regionhandle = 0;
+            if (args["destination_handle"] != null)
+                UInt64.TryParse(args["destination_handle"].AsString(), out regionhandle);
+
+            RegionInfo aRegion = new RegionInfo();
+            try
+            {
+                aRegion.UnpackRegionInfoData(args);
+            }
+            catch (Exception ex)
+            {
+                m_log.InfoFormat("[RegionPostHandler]: exception on unpacking region info {0}", ex.Message);
+                httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
+            // Finally!
+            GridRegion thisRegion = m_NeighbourService.HelloNeighbour(regionhandle, aRegion);
+
+            OSDMap resp = new OSDMap(1);
+
+            if (thisRegion != null)
+                resp["success"] = OSD.FromBoolean(true);
+            else
+                resp["success"] = OSD.FromBoolean(false);
+
+            httpResponse.RawBuffer = Util.UTF8.GetBytes(OSDParser.SerializeJsonString(resp));
+            httpResponse.StatusCode = (int)HttpStatusCode.OK;
+        }
+    }
+}
+
